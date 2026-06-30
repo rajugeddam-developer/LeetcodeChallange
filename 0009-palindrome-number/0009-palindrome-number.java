6bca77b31d64302c35d94a380f62a81520489391
Geddam Raju <rajugeddam18@gmail.com>
2026-06-30 15:34:03 +0530

    Time: 4 ms (100%), Space: 45.8 MB (72.05%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;  // negatives are not palindromes
+
+        int rev = 0;
+        int original = x;
+
+        while (x != 0) {
+            int rem = x % 10;
+            rev = (rev * 10) + rem;
+            x /= 10;
+        }
+
+        return rev == original;
+    }
+}
