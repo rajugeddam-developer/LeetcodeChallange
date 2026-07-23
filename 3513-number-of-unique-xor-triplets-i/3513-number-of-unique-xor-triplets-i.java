d4780b9ea1bc26e97bfd99100b8b44ed5ecd7422
Geddam Raju <222335639+rajugeddam-developer@users.noreply.github.com>
2026-07-23 21:28:57 +0530

    Time: 0 ms (100%), Space: 125.4 MB (26.53%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -1,7 +1,16 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
-        if (n < 3) return n;
-        return Integer.highestOneBit(n) << 1;
+
+        if (n < 3) {
+            return n;
+        }
+
+        int ans = 1;
+        while (ans <= n) {
+            ans <<= 1;
+        }
+
+        return ans;
     }
 }
