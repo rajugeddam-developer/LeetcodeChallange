0d40e2ea90a03b8a332c4e5b7c2ed108396d1068
Geddam Raju <222335639+rajugeddam-developer@users.noreply.github.com>
2026-07-28 11:51:13 +0530

    Time: 0 ms (100%), Space: 44.1 MB (74.82%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,20 +1,23 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int n=nums.length;
-        if(n==1) return 0;
-        if(nums[0]>nums[1]) return 0;
-        if(nums[n-1]>nums[n-2]) return n-1;
+        if(n==1) return 0;//if the array has single element
+        if(nums[0]>nums[1]) return 0;// if the array has two elements
+        if(nums[n-1]>nums[n-2]) return n-1;// check the last two elements
+        //Trimmmed Search Spaces
         int low=1;
         int high=n-2;
         while(low<=high){
-            int mid=low+(high-low)/2;
+            int mid=low+(high-low)/2; 
+            //compare the mid element with next element and previous element 
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
                 return mid;
             }
+            //elemenation left and rightt hald
             if(nums[mid]>nums[mid-1]){
-                low=mid+1;
+                low=mid+1;//eliminate the left half
             }else{
-                high=mid-1;
+                high=mid-1;//eleminate the right half
             }
         }
         return -1;
