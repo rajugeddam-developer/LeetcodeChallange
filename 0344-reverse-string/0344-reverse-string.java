696b6b72155634cd7f6f3502d65149049b3b6f1e
Geddam Raju <222335639+rajugeddam-developer@users.noreply.github.com>
2026-08-21 11:38:51 +0530

    Time: 0 ms (100%), Space: 48.3 MB (55.62%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,17 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left=0;
-        int right=s.length-1;
-        while(left<right){
-            char temp=s[left];
-            s[left]=s[right];
-            s[right]=temp;
-            left++;
-            right--;
+        int n=s.length;
+         
+         int low=0;
+         int high=n-1;
+        
+        while(low<=high){
+            char temp=s[low];
+            s[low]=s[high];
+            s[high]=temp;
+            low++;
+            high--;
         }
+
     }
 }
