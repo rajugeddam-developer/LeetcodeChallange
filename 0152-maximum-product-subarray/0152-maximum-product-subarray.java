1e5afc4186439fecc9f1d926f8d09fcbcf1e9206
Geddam Raju <222335639+rajugeddam-developer@users.noreply.github.com>
2026-08-23 10:30:06 +0530

    Time: 2 ms (72.29%), Space: 47.1 MB (88.58%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,22 +1,48 @@
 class Solution {
     public int maxProduct(int[] nums) {
+        // int n=nums.length;
+        // int max=Integer.MIN_VALUE;
+
+        // int left=1;
+        // int right=1;
+
+        // for(int i=0;i<n;i++){
+        //     if(left==0){
+        //         left=1;
+        //     }
+        //     if(right==0){
+        //         right=1;
+        //     }
+
+        //     left*=nums[i];
+        //     right*=nums[n-i-1];
+
+        //     max=Math.max(max,Math.max(left,right));
+        // }
+        // return max;
+ 
+
+        // Dynamic programming
         int n=nums.length;
-        int max=Integer.MIN_VALUE;
-        int prefix=1;
-        int suffix=1;
-        for(int i=0;i<n;i++){
-            if(prefix==0){
-                prefix=1;
-            }if(suffix==0){
-                suffix=1;
-            }
-            //multiply element from front
-            prefix*=nums[i];
-            //multiply elelemts form back
-            suffix*=nums[n-i-1];
-            //keep maiintain the maximum
-            max=Math.max(max,Math.max(prefix,suffix));
+
+        int maxProduct=nums[0];
+        int minProduct=nums[0];
+
+        int ans=nums[0];
+
+        for(int i=1;i<n;i++){
+            int num=nums[i];
+
+            int oldMax=maxProduct;
+            int oldMin=minProduct;
+
+            maxProduct=Math.max(num,Math.max(num*oldMax,num*oldMin));
+
+            minProduct=Math.min(num,Math.min(num*oldMax,num*oldMin));
+
+            ans=Math.max(ans,maxProduct);
         }
-        return max;
+   return ans;
+
     }
 }
