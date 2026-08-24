1e8cde478a89d74d807170a93824dfee3f019438
Geddam Raju <222335639+rajugeddam-developer@users.noreply.github.com>
2026-08-24 13:15:59 +0530

    Time: 4 ms (66.81%), Space: 92.2 MB (37.02%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+        int[] prefixSum = new int[n];
+        prefixSum[0] = stones[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + stones[i];
+        }
+
+        // Base case: player takes all stones, leaving score difference as prefixSum[n - 1]
+        int ans = prefixSum[n - 1];
+
+        // Iterate backwards from n - 2 down to 1 (since x > 1, Alice must pick at least 2 stones)
+        for (int i = n - 2; i >= 1; i--) {
+            ans = Math.max(ans, prefixSum[i] - ans);
+        }
+
+        return ans;
+    }
+}
